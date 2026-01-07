ce59a18d319edf3f2e0e341faab7d487a16ab85f
serhiipelypenko <sergey.pelipenko@triogeeks.com>
2026-01-07 19:20:06 +0200

    Init laralve + reverb

---
+++ reverb/vite.config.ts
@@ -0,0 +1,35 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    server: {
+        host: '0.0.0.0', // Bind to all interfaces for Docker
+        port: 5173,      // Explicitly set the port
+        hmr: {
+            host: 'localhost', // Use 'localhost' for HMR in the browser
+            protocol: 'ws',    // Ensure WebSocket for HMR
+        },
+    },
+});
